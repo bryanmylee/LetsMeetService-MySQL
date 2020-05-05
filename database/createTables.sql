75456172506992f44bd3e011e393ed0e0131b2ec
Bryan Lee <bryanleeminyuan@gmail.com>
2020-05-05 13:59:29 +0800

    Simplify database design

--- database/createTables.sql
+++ database/createTables.sql
@@ -3,24 +3,13 @@ CREATE DATABASE lets_meet;
 USE lets_meet;
 SET GLOBAL time_zone = '+00:00';
 
-
-CREATE TABLE `user` (
-    id           INT          NOT NULL AUTO_INCREMENT,
-    username     VARCHAR(255) NOT NULL UNIQUE, INDEX(username(10)),
-    display_name VARCHAR(255),
-    password     BINARY(60)   NOT NULL, /* Bcrypt hash */
-    PRIMARY KEY (id)
-);
-
 CREATE TABLE `event` (
     id           INT          NOT NULL AUTO_INCREMENT,
     url_id       VARCHAR(255) NOT NULL UNIQUE, INDEX(url_id(10)),
     title        VARCHAR(255) NOT NULL,
     description  VARCHAR(255) NOT NULL DEFAULT "",
     date_created DATETIME     NOT NULL DEFAULT NOW(),
-    owner_id     INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (owner_id) REFERENCES `user`(id) ON DELETE SET NULL
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE `event_timing` (
@@ -31,37 +20,21 @@ CREATE TABLE `event_timing` (
     FOREIGN KEY (event_id) REFERENCES `event`(id) ON DELETE CASCADE
 );
 
-CREATE TABLE `temp_user` (
-    event_id  INT          NOT NULL,
-    temp_name VARCHAR(255) NOT NULL,
-    password  BINARY(60)   NOT NULL,
-    PRIMARY KEY (event_id, temp_name),
+CREATE TABLE `event_user` (
+    event_id INT          NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password BINARY(60)   NOT NULL,
+    is_admin BOOLEAN      NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (event_id, username),
     FOREIGN KEY (event_id) REFERENCES `event`(id) ON DELETE CASCADE
 );
 
-CREATE TABLE `temp_timing` (
+CREATE TABLE `timing` (
     event_id    INT          NOT NULL,
-    temp_name   VARCHAR(255) NOT NULL,
+    username    VARCHAR(255) NOT NULL,
     start_dtime DATETIME     NOT NULL,
     end_dtime   DATETIME     NOT NULL,
-    PRIMARY KEY (event_id, temp_name, start_dtime),
-    FOREIGN KEY (event_id, temp_name) REFERENCES `temp_user`(event_id, temp_name) ON DELETE CASCADE
-);
-
-CREATE TABLE `schedule` (
-    event_id INT NOT NULL,
-    user_id  INT NOT NULL,
-    PRIMARY KEY (event_id, user_id),
-    FOREIGN KEY (event_id) REFERENCES `event`(id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES `user`(id) ON DELETE CASCADE
-);
-
-CREATE TABLE `timing` (
-    event_id    INT      NOT NULL,
-    user_id     INT      NOT NULL,
-    start_dtime DATETIME NOT NULL,
-    end_dtime   DATETIME NOT NULL,
-    PRIMARY KEY (event_id, user_id, start_dtime),
-    FOREIGN KEY (event_id, user_id) REFERENCES `schedule`(event_id, user_id) ON DELETE CASCADE
+    PRIMARY KEY (event_id, username, start_dtime),
+    FOREIGN KEY (event_id, username) REFERENCES `event_user`(event_id, username) ON DELETE CASCADE
 );
 
--- database/testData.sql
+++ database/testData.sql
@@ -1,12 +1,8 @@
 USE lets_meet;
 
-INSERT INTO `user`(id, username, display_name, password) VALUES
-(1, "adam", "Adam", "$2b$10$U0tDjy066EGn43ofqQAaKu.K6i278wcT0o7ypLi/FfHh6o496cUuS"),
-(2, "bran", "Bran", "$2b$10$gt7PSFaSn1K0YJhqRn1xgOe5DgiM65ji8yiQDfTq/P0A0vKA4QMMa");
-
-INSERT INTO `event`(id, url_id, title, description, date_created, owner_id) VALUES
+INSERT INTO `event`(id, url_id, title, description, date_created) VALUES
 (1, "GraciousSnarlingHorse", "First event", "An event to test the database",
-    "2020-05-05 04:00:00", 1); /* 2020-05-05 12pm+8*/
+    "2020-05-05 04:00:00"); /* 2020-05-05 12pm+8*/
 
 INSERT INTO `event_timing`(event_id, start_dtime, end_dtime) VALUES
 /* 12pm+8 to 4pm+8 on 6 May 2020 */
@@ -16,27 +12,24 @@ INSERT INTO `event_timing`(event_id, start_dtime, end_dtime) VALUES
 /* 12pm+8 to 4pm+8 on 7 May 2020 */
 (1, "2020-05-07 04:00:00", "2020-05-06 08:00:00");
 
-INSERT INTO `schedule`(event_id, user_id) VALUES
-(1, 1),
-(1, 2);
+INSERT INTO `event_user`(event_id, username, password, is_admin) VALUES
+(1, "adam", "$2b$10$U0tDjy066EGn43ofqQAaKu.K6i278wcT0o7ypLi/FfHh6o496cUuS", TRUE),
+(1, "bran", "$2b$10$gt7PSFaSn1K0YJhqRn1xgOe5DgiM65ji8yiQDfTq/P0A0vKA4QMMa", FALSE),
+(1, "cory", "$2b$10$mKNZc0.Z6mXsaSBaDAuxEeNSrRfVl0Vg6xCmFJ8hoJOHRxVbmxR6e", FALSE);
 
-INSERT INTO `timing`(event_id, user_id, start_dtime, end_dtime) VALUES
+INSERT INTO `timing`(event_id, username, start_dtime, end_dtime) VALUES
 /* Adam's timings */
 /* 12pm+8 to 4pm+8 on 6 May 2020 */
-(1, 1, "2020-05-06 04:00:00", "2020-05-06 08:00:00"),
+(1, "adam", "2020-05-06 04:00:00", "2020-05-06 08:00:00"),
 /* 6pm+8 to 10pm+8 on 6 May 2020 */
-(1, 1, "2020-05-06 10:00:00", "2020-05-06 14:00:00"),
+(1, "adam", "2020-05-06 10:00:00", "2020-05-06 14:00:00"),
 /* 12pm+8 to 4pm+8 on 7 May 2020 */
-(1, 1, "2020-05-07 04:00:00", "2020-05-06 08:00:00"),
+(1, "adam", "2020-05-07 04:00:00", "2020-05-06 08:00:00"),
 /* Bran's timings */
 /* 12pm+8 to 2pm+8 on 6 May 2020 */
-(1, 2, "2020-05-06 04:00:00", "2020-05-06 06:00:00"),
+(1, "bran", "2020-05-06 04:00:00", "2020-05-06 06:00:00"),
 /* 12pm+8 to 2pm+8 on 7 May 2020 */
-(1, 2, "2020-05-07 04:00:00", "2020-05-07 06:00:00");
-
-INSERT INTO `temp_user`(event_id, temp_name, password) VALUES
-(1, "Cory", "$2b$10$mKNZc0.Z6mXsaSBaDAuxEeNSrRfVl0Vg6xCmFJ8hoJOHRxVbmxR6e");
-
-INSERT INTO `temp_timing`(event_id, temp_name, start_dtime, end_dtime) VALUES
+(1, "bran", "2020-05-07 04:00:00", "2020-05-07 06:00:00"),
+/* Cory's timings */
 /* 6pm+8 to 8pm+8 on 6 May 2020 */
 (1, "Cory", "2020-05-06 10:00:00", "2020-05-06 12:00:00");
