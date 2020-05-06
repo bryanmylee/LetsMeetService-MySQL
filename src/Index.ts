f04c8e44a583a691ee2dca963216104be7714640
Bryan Lee <bryanleeminyuan@gmail.com>
2020-05-06 16:51:22 +0800

    Shift from 4 to 2 space tabs

--- src/Index.ts
+++ src/Index.ts
@@ -3,37 +3,37 @@ import express from 'express';
 const app: express.Application = express();
 
 app.post('/new', (req, res) => {
-    res.send('Creating a new event.');
+  res.send('Creating a new event.');
 });
 
 app.get('/:eventId', (req, res) => {
-    const { eventId } = req.params;
-    res.send(`eventId: ${eventId}`);
+  const { eventId } = req.params;
+  res.send(`eventId: ${eventId}`);
 });
 
 app.post('/:eventId/login', (req, res) => {
-    res.send('Verifying user credentials...');
+  res.send('Verifying user credentials...');
 });
 
 app.post('/:eventId/refresh_token', (req, res) => {
-    res.send('Issuing new access and refresh tokens...');
+  res.send('Issuing new access and refresh tokens...');
 });
 
 app.post('/:eventId/:username/edit', (req, res) => {
-    // Check for access token
-    const { eventId, username } = req.params;
-    res.send(`Updating user: ${username} information on eventId: ${eventId}.`);
+  // Check for access token
+  const { eventId, username } = req.params;
+  res.send(`Updating user: ${username} information on eventId: ${eventId}.`);
 });
 
 app.post('/:eventId/:username', (req, res) => {
-    const { eventId, username } = req.params;
-    res.send(`Adding new user: ${username} on eventId: ${eventId}.`);
+  const { eventId, username } = req.params;
+  res.send(`Adding new user: ${username} on eventId: ${eventId}.`);
 });
 
 app.get('/', (_, res) => {
-    res.send('LetsMeet Web Service.');
+  res.send('LetsMeet Web Service.');
 });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`);
+  console.log(`Listening on port ${process.env.PORT}`);
 });
