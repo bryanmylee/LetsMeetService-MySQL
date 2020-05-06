25906ad046b83dc26b2dd6864de1838606301bee
Bryan Lee <bryanleeminyuan@gmail.com>
2020-05-06 16:26:46 +0800

    Set up REST routes

--- src/Index.ts
+++ src/Index.ts
@@ -2,10 +2,38 @@ import express from 'express';
 
 const app: express.Application = express();
 
-app.get('/', (_, res) => {
-    res.send('Hello world!');
+app.post('/new', (req, res) => {
+    res.send('Creating a new event.');
 });
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+app.get('/:eventId', (req, res) => {
+    const { eventId } = req.params;
+    res.send(`eventId: ${eventId}`);
+});
+
+app.post('/:eventId/login', (req, res) => {
+    res.send('Verifying user credentials...');
+});
+
+app.post('/:eventId/refresh_token', (req, res) => {
+    res.send('Issuing new access and refresh tokens...');
+});
+
+app.post('/:eventId/:username/edit', (req, res) => {
+    // Check for access token
+    const { eventId, username } = req.params;
+    res.send(`Updating user: ${username} information on eventId: ${eventId}.`);
+});
+
+app.post('/:eventId/:username', (req, res) => {
+    const { eventId, username } = req.params;
+    res.send(`Adding new user: ${username} on eventId: ${eventId}.`);
+});
+
+app.get('/', (_, res) => {
+    res.send('LetsMeet Web Service.');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
 });
