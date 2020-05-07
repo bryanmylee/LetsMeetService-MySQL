48a18b7312c0def110c912277a199074f306d56b
Bryan Lee <bryanleeminyuan@gmail.com>
2020-05-08 02:13:38 +0800

    Implement logout route

--- src/index.ts
+++ src/index.ts
@@ -38,74 +38,6 @@ app.post('/new', async (req, res, next) => {
   }
 });
 
-app.get('/:eventUrl', async (req, res, next) => {
-  try {
-    const { eventUrl } = req.params;
-
-    const session = await client.getSession();
-    const event = await database.getEvent(session, eventUrl);
-    res.send(event);
-  } catch (err) {
-    next(err);
-  }
-});
-
-app.post('/:eventUrl/login', async (req, res, next) => {
-  try {
-    const { eventUrl } = req.params;
-    const { username, password }: {
-      username: string, password: string,
-    } = req.body;
-
-    const session = await client.getSession();
-    const eventId = await database.getId(session, eventUrl);
-    const storedHash = await database.getUserCredentials(
-        session, eventId, username);
-    if (storedHash === null) throw new Error('User not found.');
-
-    const valid = await bcrypt.compare(password, storedHash);
-    if (!valid) throw new Error('Password invalid');
-    await login(session, res, eventId, eventUrl, username);
-  } catch (err) {
-    console.log(err);
-    res.status(400);
-    res.send({
-      error: err.message
-    });
-  }
-});
-
-app.post('/:eventUrl/refresh_token', async (req, res, next) => {
-  try {
-    const { eventUrl } = req.params;
-    const { refreshToken }: { refreshToken: string } = req.cookies;
-    if (refreshToken == null) throw new Error('Refresh token not found.');
-
-    // Verify that the token is not tampered with, and retrieve the payload.
-    const payload: {
-      uid: string,
-      evt: string,
-    } = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET!) as any;
-
-    const session = await client.getSession();
-    const eventId = await database.getId(session, eventUrl);
-    const storedToken = await database.getUserRefreshToken(
-        session, eventId, payload.uid);
-    if (storedToken == null) throw new Error('User invalid.');
-    if (storedToken !== refreshToken) throw new Error('Refresh token invalid');
-
-    login(session, res, eventId, eventUrl, payload.uid);
-  } catch (err) {
-    next(err);
-  }
-});
-
-app.post('/:eventUrl/:username/edit', (req, res) => {
-  // Check for access token
-  const { eventUrl, username } = req.params;
-  res.send(`Updating user: ${username} information on eventUrl: ${eventUrl}.`);
-});
-
 app.post('/:eventUrl/new_user', async (req, res, next) => {
   try {
     const { eventUrl } = req.params;
@@ -133,6 +65,85 @@ app.post('/:eventUrl/new_user', async (req, res, next) => {
         code: info.code,
       });
     }
+    next(err);
+  }
+});
+
+app.post('/:eventUrl/login', async (req, res) => {
+  try {
+    const { eventUrl } = req.params;
+    const { username, password }: {
+      username: string, password: string,
+    } = req.body;
+
+    const session = await client.getSession();
+    const eventId = await database.getId(session, eventUrl);
+    const storedHash = await database.getUserCredentials(
+        session, eventId, username);
+    if (storedHash === null) throw new Error('User not found.');
+
+    const valid = await bcrypt.compare(password, storedHash);
+    if (!valid) throw new Error('Password invalid');
+    await login(session, res, eventId, eventUrl, username);
+  } catch (err) {
+    console.log(err);
+    res.status(400);
+    res.send({
+      error: err.message,
+    });
+  }
+});
+
+app.post('/:eventUrl/logout', async (req, res) => {
+  const { eventUrl } = req.params;
+  res.clearCookie('refreshToken', { path: `/${eventUrl}/refresh_token` });
+  res.send({
+    message: 'Logged out',
+  });
+});
+
+app.post('/:eventUrl/refresh_token', async (req, res, next) => {
+  try {
+    const { eventUrl } = req.params;
+    const { refreshToken }: { refreshToken: string } = req.cookies;
+    if (refreshToken == null) throw new Error('Refresh token not found.');
+
+    // Verify that the token is not tampered with, and retrieve the payload.
+    const payload: {
+      uid: string,
+      evt: string,
+    } = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET!) as any;
+
+    const session = await client.getSession();
+    const eventId = await database.getId(session, eventUrl);
+    const storedToken = await database.getUserRefreshToken(
+        session, eventId, payload.uid);
+    if (storedToken == null) throw new Error('User invalid.');
+    if (storedToken !== refreshToken) throw new Error('Refresh token invalid');
+
+    login(session, res, eventId, eventUrl, payload.uid);
+  } catch (err) {
+    res.status(400);
+    res.send({
+      error: err.message,
+    })
+  }
+});
+
+app.post('/:eventUrl/:username/edit', (req, res) => {
+  // Check for access token
+  const { eventUrl, username } = req.params;
+  res.send(`Updating user: ${username} information on eventUrl: ${eventUrl}.`);
+});
+
+app.get('/:eventUrl', async (req, res, next) => {
+  try {
+    const { eventUrl } = req.params;
+
+    const session = await client.getSession();
+    const event = await database.getEvent(session, eventUrl);
+    res.send(event);
+  } catch (err) {
     next(err);
   }
 });
