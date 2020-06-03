1ed7c5848c5fb4bfac81ed02d0635af526dfba8a
Bryan Lee <bryanmylee@gmail.com>
2020-06-03 14:09:08 +0800

    Add configuration for database socket and port

--- src/database/index.ts
+++ src/database/index.ts
@@ -9,12 +9,14 @@ import {
 } from './userAccess';
 
 const {
-  DB_HOST, DB_NAME, DB_USER, DB_PASS,
+  DB_HOST, DB_PORT, DB_SOCKET, DB_NAME, DB_USER, DB_PASS,
   DB_POOL_IDLE, DB_POOL_SIZE, DB_POOL_QUEUE_TIMEOUT
 } = process.env;
 
 const connConfig = {
   host: DB_HOST,
+  port: DB_PORT,
+  socket: DB_SOCKET,
   schema: DB_NAME,
   user: DB_USER,
   password: DB_PASS,
